7d26c8211c849b08a314aeadaa30937e0860e77d
Luiz Davi Vaz <143763800+vazimm@users.noreply.github.com>
2025-02-03 22:02:21 -0300

    Msg de confirmação da compra

--- js/app.js
+++ js/app.js
@@ -9,19 +9,22 @@ function comprar(){
     if (tipo_ingresso === "pista") {
         if (quantidade_ingresso > pista_qtd) {
             return alert("Não há ingressos suficientes disponíveis para Pista.");
-        }    
+        }   
+            alert("compra realizada com sucesso!") 
             const valor = pista_qtd - quantidade_ingresso;
             document.getElementById("qtd-pista").innerHTML = valor;
     } else if (tipo_ingresso === "superior"){
         if (quantidade_ingresso > cadeira_superior_qtd){
             return alert("Não há ingressos suficientes disponíveis para Cadeiras superiores");
-        }
+        }   
+        alert("compra realizada com sucesso!")
         const valor = cadeira_superior_qtd - quantidade_ingresso;
         document.getElementById("qtd-superior").innerHTML = valor;
     } else if (tipo_ingresso === "inferior"){
         if (quantidade_ingresso > cadeira_inferior_qtd){
             return alert("Não há ingressos suficientes disponíveis para Cadeiras inferiores");
         }
+        alert("compra realizada com sucesso!")
         const valor = cadeira_inferior_qtd - quantidade_ingresso;
         document.getElementById("qtd-inferior").innerHTML = valor;
     } else {
